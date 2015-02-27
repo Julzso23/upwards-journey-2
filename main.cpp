2280be55bd714d29b1609a629cb6965adf660c0a
Julxzs <julzso23@gmail.com>
2015-02-27 23:40:16 +0000

    Change void main to int main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "Game.h"
 #include "UJ.h"
 
-void main()
+int main()
 {
 	UJ upwardsJourney("Upwards Journey 2", sf::VideoMode::getDesktopMode(), true);
-
 	upwardsJourney.run();
+
+	return 0;
 }
