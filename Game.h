f9e340601bc39989abfd4933ff47803208eed528
Julxzs <julzso23@gmail.com>
2015-04-04 12:55:38 +0100

    Moved virtual functions to private

--- Game.h
+++ Game.h
@@ -12,6 +12,20 @@ private:
 	std::vector<sf::VideoMode> supportedResolutions;
 	sf::Clock time;
 	sf::View camera;
+
+	virtual void load() = 0;
+	virtual void update(float dt) = 0;
+	virtual void draw(sf::RenderWindow* window) = 0;
+	virtual void drawUnscaled(sf::RenderWindow* window) = 0;
+	virtual void mousePressed(sf::Mouse::Button button, sf::Vector2i position) = 0;
+	virtual void mouseReleased(sf::Mouse::Button button, sf::Vector2i position) = 0;
+	virtual void mouseMoved(int x, int y) = 0;
+	virtual void keyPressed(sf::Keyboard::Key key) = 0;
+	virtual void keyReleased(sf::Keyboard::Key key) = 0;
+	virtual void joystickConnected(int id) = 0;
+	virtual void joystickDisconnected(int id) = 0;
+	virtual void windowResized(sf::Vector2u size) = 0;
+	virtual void onExit() = 0;
 public:
 	Game(sf::String title, sf::VideoMode size, bool fullscreen);
 	void run();
@@ -29,20 +43,6 @@ public:
 	sf::Vector2i mousePos();
 
 	void exit();
-
-	virtual void load() = 0;
-	virtual void update(float dt) = 0;
-	virtual void draw(sf::RenderWindow* window) = 0;
-	virtual void drawUnscaled(sf::RenderWindow* window) = 0;
-	virtual void mousePressed(sf::Mouse::Button button, sf::Vector2i position) = 0;
-	virtual void mouseReleased(sf::Mouse::Button button, sf::Vector2i position) = 0;
-	virtual void mouseMoved(int x, int y) = 0;
-	virtual void keyPressed(sf::Keyboard::Key key) = 0;
-	virtual void keyReleased(sf::Keyboard::Key key) = 0;
-	virtual void joystickConnected(int id) = 0;
-	virtual void joystickDisconnected(int id) = 0;
-	virtual void windowResized(sf::Vector2u size) = 0;
-	virtual void onExit() = 0;
 };
 
 #endif
